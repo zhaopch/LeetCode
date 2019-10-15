52a4248caca792827357bcd2057834d72e146720
zhaopengcheng <zhaopengcheng@10.222.10.5>
2019-10-15 15:50:07 +0800

    leetcode No.2 addTwoNumbers

---
+++ src/com/zhaopch/algorithm/LeetCode002.java
@@ -0,0 +1,71 @@
+package com.zhaopch.algorithm;
+
+/**
+ *
+ * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+ * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ *
+ * @author PengCheng.Zhao
+ */
+public class LeetCode002 {
+	public static void main(String[] args) {
+		ListNode l1, l2;
+		l1 = new ListNode(5);
+		l1.next = new ListNode(2);
+		l1.next.next = new ListNode(0);
+		l1.next.next.next = new ListNode(3);
+		l2 = new ListNode(8);
+		l2.next = new ListNode(3);
+		l2.next.next = new ListNode(1);
+		ListNode l3 = addTwoNumbers(l1, l2);
+		System.out.println("L1: " + l1.toString());
+		System.out.println("L2: " + l2.toString());
+		System.out.println("L3: " + l3.toString());
+	}
+
+	static class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode(int x) {
+			val = x;
+		}
+
+		@Override
+		public String toString() {
+			// TODO Auto-generated method stub
+			String result = "";
+			if (next == null) {
+				result += val;
+			} else {
+				result += val + "->" + next.toString();
+			}
+			return result;
+		}
+	}
+
+	public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+		ListNode dummyHead = new ListNode(0);
+		ListNode p = l1, q = l2, curr = dummyHead;
+		int carry = 0;
+		while (p != null || q != null) {
+			int x = (p != null) ? p.val : 0;
+			int y = (q != null) ? q.val : 0;
+			int sum = carry + x + y;
+			carry = sum / 10;
+			curr.next = new ListNode(sum % 10);
+			curr = curr.next;
+			if (p != null) {
+				p = p.next;
+			}
+			if (q != null) {
+				q = q.next;
+			}
+		}
+		if (carry > 0) {
+			curr.next = new ListNode(carry);
+		}
+		return dummyHead.next;
+
+	}
+}
